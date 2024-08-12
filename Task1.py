62fe3f29f72fd843c40e545ab95bbf5576b7a534
Pratik Ramagare <164848357+PrexSide@users.noreply.github.com>
2024-08-12 11:06:22 +0530

    MainFLow TASK 01
    "Successfully completed a data science task on GitHub, delving into the basics of data types like lists, dictionaries, and sets. Enhanced my skills in organizing and managing code while contributing to my growing repository of knowledge."

---
+++ Task1.py
@@ -0,0 +1,17 @@
+#Creating a list which inculde Top Company names 
+my_list = [ 18,45,12,100 ]
+print("Initial List:", my_list)
+
+#Adding an element to the list
+my_list.append(22)
+print("New List after addition of an Element:", my_list)
+
+#Remove an element fron the list
+my_list.remove(100)
+print("New List after Removing an Element:", my_list)
+
+#Modifiying An Elemet in the list
+my_list[2] = 101
+print("New List after Modifiying an Element:", my_list)
+
+print(" Updated List :", my_list)
---
+++ Task3.py
@@ -0,0 +1,21 @@
+#Creating a Set 
+my_set = { 2,7,12,29,}
+print("Initial Set:", my_set)
+
+##Adding an Element in the Set
+my_set.add(17)
+print("\nNew Set after addition of an Element:", my_set)
+
+#Removing an Element in the  Set
+my_set.remove(7)
+print("\nNew Set after Removing an Element:", my_set)
+
+#Modifiying an Element in the Set
+my_set.discard(12)
+my_set.add(21)
+print("\nNew Set after Modifiying an Element:", my_set)
+
+print("\nUpdated Set :", my_set)
+
+
+
